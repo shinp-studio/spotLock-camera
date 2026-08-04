8c374833e61bbbd004dfa0b1b8e8d755745826cc
smatsutani <dear.old.days@gmail.com>
2026-08-04 16:14:49 +0900

    chore: bump Android app version to 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.shinpstudio.spotlockcamera"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
