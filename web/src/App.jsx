281d0bdbf61a1743056e5af5a3c6d812d74c0268
smatsutani <dear.old.days@gmail.com>
2026-08-04 13:42:28 +0900

    refactor: remove legacy web directory (migrated to spotlock-school-tools)

--- web/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Dashboard from './components/Dashboard'
-import './styles/light-theme.css'
-
-export default function App() {
-  return (
-    <Dashboard />
-  )
-}
